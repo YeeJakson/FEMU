82c41fa5ee98558f0d51c6f43eae213a65d3a62b
yihao <2268080550@qq.com>
2024-05-05 15:24:03 +0000

    modify tt_lines

--- hw/femu/bbssd/ftl.c
+++ hw/femu/bbssd/ftl.c
@@ -240,7 +240,8 @@ static void ssd_init_params(struct ssdparams *spp)
     spp->secsz = 512;
     spp->secs_per_pg = 8;
     spp->pgs_per_blk = 256;
-    spp->blks_per_pl = 256; /* 16GB */
+    // spp->blks_per_pl = 256; /* 16GB */
+    spp->blks_per_pl = 1063;
     spp->pls_per_lun = 1;
     spp->luns_per_ch = 8;
     spp->nchs = 8;
